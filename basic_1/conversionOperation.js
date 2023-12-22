d94b8bf9476925b3ceddc609d39b1ed6910032f1
Bibek Upreti <bibekupreti1998@gmail.com>
2023-12-22 14:36:30 +0100

    conversion done

---
+++ basic_1/conversionOperation.js
@@ -0,0 +1,24 @@
+let age = true
+
+
+console.log(typeof age)
+console.log(typeof (age))
+
+
+let valueInNumber = Number(age)
+console.log(typeof (valueInNumber))
+console.log(valueInNumber)
+
+
+/* 
+ "330" => 330
+ "33abc" => NaN
+ true =1 || false = 0
+
+*/
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ basic_1/datatyp.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+
+//alert("3+3") // we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 19
+let isLoggedIn = false
+
+/*
+number
+bigint
+string
+boolean
+null = standalone value
+undefined
+symbol = unique
+*/
+
+//Object
+
+
+console.log(typeof age)
