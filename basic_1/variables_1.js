b2bd525ad9851cd072c711af7e33ca54077b17b3
Bibek Upreti <bibekupreti1998@gmail.com>
2023-12-22 14:16:06 +0100

    variables learnt

---
+++ basic_1/variables_1.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "bibekupreti02@google.com"
+var accountPassword ="1998"
+accountCity = "Hetauda"
+let accountState
+
+//accountId = 2 //not allowed
+accountEmail = "abcd"
+accountPassword = "123449"
+accountCity = "kathmandu"
+console.log(accountId);
+/* prefer not to use var
+because of issue in block scope and functional scope 
+ */
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
